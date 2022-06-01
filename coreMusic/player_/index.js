a4c998ba33ea4376634be812e74fc96a3d6a4c61
rcph <rcph110110@gmail.com>
2022-06-02 02:34:14 +0800

    done

--- coreMusic/player_/index.js
+++ coreMusic/player_/index.js
@@ -1,4 +1,5 @@
         let plist_vl = []
+        let pathList = []
         let currentPlay = null
         let currentAudio = new Audio()
 
@@ -18,16 +19,25 @@
 
         setInterval(() => {
             if(currentAudio.ended && plist_vl[0] != undefined){
-                initialize_list(plist_vl[0])
-                audio_play()
-                display_title()
-                display_icon(plist_vl[0])
-                display_media(plist_vl[0])
-                shift_playlist()
+                for(data of pathList){
+                    for(let k = 0; k < data[1].length; ++k){
+                        if(`./${data[0]}/${data[1][k]}` == `./${data[0]}/${plist_vl[0]}`){
+                            initialize_list(plist_vl[0],data[0])
+                            audio_play()
+                            display_title()
+                            display_icon(plist_vl[0],data[0])
+                            display_media()
+                            shift_playlist()
+                        }
+                    }
+                    
+                }
             }
         }, 1000)
         
-        function add_ctr_lists(lists,folder_name) {
+        function add_ctr_lists(lists,folder_name = "") {
+            pathList.push([folder_name,lists])
+
             const ctr_lists = document.createElement("div")
             ctr_lists.setAttribute("class","ctr-lists")
 
@@ -78,6 +88,17 @@
                     })
                 }
             }
+            //intersection observer ----------------------------
+            const ctr_glist = document.querySelectorAll(".ctr-list")
+            const ctr_list_observer = new IntersectionObserver(entries => {
+                entries.forEach(entry => {
+                    entry.target.classList.toggle("manifest-ctr-list",entry.isIntersecting)
+                })
+            })
+            ctr_glist.forEach((ctr) => {
+                ctr_list_observer.observe(ctr)
+            })
+        // --------------------------------------------------
         }
         function add_ctr_h(folder_name){
             const ctr_h = document.createElement("div")
@@ -95,36 +116,6 @@
             add_ctr_h(folder_name)
             add_ctr_lists(lists,folder_name)
         }
-        ctr_template_scroller([
-            "f(x) - 4 Walls (Zekk Remix).mp4",
-"greyl - Trendy.mp4",
-"kamome sano - Lovesick (feat ぷにぷに電機).mp4",
-"OYASUMI in my dream (Original Mix).mp4",
-"PSYQUI & Houou Karin - Shining Lights  Speed Garage.mp4",
-"Moe Shop - Baby Pink (w YUCe).mp4",
-"Nightcore - No Friends (Lyrics).mp4",
-"PSYQUI - Are You Kidding Me (ft Mami)  Jpop Future Core.mp4",
-"PSYQUI - Fly to the moon feat 中村さんそ.mp4",
-"PSYQUI - Hype feat Such.mp4",
-"PSYQUI - dont you want me ft Such.mp4",
-"PSYQUI - Secret Dance Hall (ft SUCH)  J-pop 2019.mp4",
-"PSYQUI - Multitalents.mp4",
-"PSYQUI - OYASUMI in my dream.mp4",
-"PSYQUI - Still in my heart feat ぷにぷに電機  Lyrics [CC].mp4",
-"PSYQUI - ヒステリックナイトガール (Awakening) (ft SUCH)  J-Pop.mp4",
-"PSYQUI feat Marpril - Girly Cupid.mp4",
-"PSYQUI - Your Voice So Zekk Full Spec Remix (Ft SUCH)  Future Core.mp4",
-"Still in my heart (feat ぷにぷに電機).mp4",
-"Signal (feat Such).mp4",
-"Rejection - Signal (ft SUCH)  Future Core 2020.mp4",
-"Your voice so (feat such).mp4",
-"Ujico  Snails House - Pixel Dream.mp4",
-"Zakku x Nakanojojo - Matcha Love (feat アリガトユイナ).mp4",
-"[Blue Archive] Theme 109.mp4",
-"[Future Garage] f(x) - 4 Walls (Zekk Remix).mp4",
-"PSYQUI - ヒステリックナイトガール feat Such (android52 Edit).mp4",
-"「Footwork」[PSYQUI] Too Late.mp4"
-        ],"MaidCat")
 
         function add_playlist(list,folder_name) {
             plist_vl.push(list)
@@ -151,18 +142,6 @@
             sd_wrapper.children[0].remove()
         }
 
-        //intersection observer ----------------------------
-        const ctr_glist = document.querySelectorAll(".ctr-list")
-        const ctr_list_observer = new IntersectionObserver(entries => {
-            entries.forEach(entry => {
-                entry.target.classList.toggle("manifest-ctr-list",entry.isIntersecting)
-            })
-        })
-        ctr_glist.forEach((ctr) => {
-            ctr_list_observer.observe(ctr)
-        })
-        // --------------------------------------------------
-
         function ctrPlay(list,folder_name) {
             if(list == currentPlay && !currentAudio.paused) return
             else if(list == currentPlay && currentAudio.paused){
@@ -172,8 +151,8 @@
                 initialize_list(list,folder_name)
                 audio_status()
                 display_title()
-                display_icon(list)
-                display_media(list)
+                display_icon(list,folder_name)
+                display_media()
             }
         }
 
@@ -213,8 +192,8 @@
         function display_title(file_name){
             title.textContent = currentPlay
         }
-        function display_icon(icon){
-            curr_icon.src = `./${folder_name}/${list}`
+        function display_icon(icon,folder_name){
+            curr_icon.src = `./${folder_name}/${icon}`
             curr_icon.play()
             curr_icon.muted = true
             setTimeout(() => {
@@ -224,7 +203,6 @@
         }
         function display_media(list) {
             media.muted = true
-            media.play()
         }
         function thumb_status() {
             
@@ -254,3 +232,43 @@
         // range.addEventListener("timeupdate",thumb_status)
         curr_player.addEventListener("click",audio_status)
         sd_btn.addEventListener("click",sd_switch)
+
+        ctr_template_scroller([
+            "f(x) - 4 Walls (Zekk Remix).mp4",
+"greyl - Trendy.mp4",
+"kamome sano - Lovesick (feat ぷにぷに電機).mp4",
+"OYASUMI in my dream (Original Mix).mp4",
+"PSYQUI & Houou Karin - Shining Lights  Speed Garage.mp4",
+"Moe Shop - Baby Pink (w YUCe).mp4",
+"Nightcore - No Friends (Lyrics).mp4",
+"PSYQUI - Are You Kidding Me (ft Mami)  Jpop Future Core.mp4",
+"PSYQUI - Fly to the moon feat 中村さんそ.mp4",
+"PSYQUI - Hype feat Such.mp4",
+"PSYQUI - dont you want me ft Such.mp4",
+"PSYQUI - Secret Dance Hall (ft SUCH)  J-pop 2019.mp4",
+"PSYQUI - Multitalents.mp4",
+"PSYQUI - OYASUMI in my dream.mp4",
+"PSYQUI - Still in my heart feat ぷにぷに電機  Lyrics [CC].mp4",
+"PSYQUI - ヒステリックナイトガール (Awakening) (ft SUCH)  J-Pop.mp4",
+"PSYQUI feat Marpril - Girly Cupid.mp4",
+"PSYQUI - Your Voice So Zekk Full Spec Remix (Ft SUCH)  Future Core.mp4",
+"Still in my heart (feat ぷにぷに電機).mp4",
+"Signal (feat Such).mp4",
+"Rejection - Signal (ft SUCH)  Future Core 2020.mp4",
+"Your voice so (feat such).mp4",
+"Ujico  Snails House - Pixel Dream.mp4",
+"Zakku x Nakanojojo - Matcha Love (feat アリガトユイナ).mp4",
+"[Blue Archive] Theme 109.mp4",
+"[Future Garage] f(x) - 4 Walls (Zekk Remix).mp4",
+"PSYQUI - ヒステリックナイトガール feat Such (android52 Edit).mp4",
+"「Footwork」[PSYQUI] Too Late.mp4"
+        ],"MaidCat")
+        ctr_template_scroller(["Bad Computer & Danyka Nadeau - Chasing Clouds.mp4",
+        "Just Love (feat PSYQUI).mp4",
+        "Kunaru & Kachi - One More Time.mp4",
+        "masara - Tegami (ft yutsuki & Hikaru Station).mp4",
+        "PSYQUI & Houou Karin - Shining Lights  Speed Garage.mp4",
+        "PSYQUI - Beautiful Future.mp4",
+        "PSYQUI - Education.mp4",
+        "Zekk - City Lights (Remix).mp4",
+        ],"Nyahn")
