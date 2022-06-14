46fda8c3730da4ff07b73908a109c37a4426bfcc
rcph <rcph110110@gmail.com>
2022-06-14 23:34:10 +0800

    done

--- coreMusic/player_/index.js
+++ coreMusic/player_/index.js
@@ -16,7 +16,7 @@
 
         const playlist_icon = document.querySelector(".playlist-icon")
         const playlist_holder = document.querySelector(".playlist-holder")
-
+        
         async function plist_promise() {
             try {
                 pathdata = sd_wrapper.children[0].children[0].src.split("/")
@@ -24,7 +24,10 @@
                 if(currentAudio.ended && plist_vl[0] != undefined){
                     for(data of pathList){
                         for(let k = 0; k < data[1].length; ++k){
-                            if(`${data[0]}/${data[1][k]}` == `${data[0]}/${plist_vl[0]}` && plist_vl[0] == decodeURI(pathdata[pathdata.length - 1])){
+                            if(`${data[0]}/${data[1][k]}` == `${data[0]}/${plist_vl[0]}` && decodeURI(plist_vl[0]) == decodeURI(pathdata[pathdata.length - 1])){
+                                    // preload_auto()
+                                    sd_wrapper.children[0].children[0].preload = "auto"
+
                                     initialize_list(plist_vl[0],data[0])
                                     await audio_play()
                                     display_title()
@@ -229,7 +232,6 @@
             function playlist_screen_out() {
                 plist_ilist.currentTime = 10
             }
-            
             //bug 
             const plist_ilist_observer = new IntersectionObserver(entries => {
                 entries.forEach(entry => {
@@ -248,9 +250,7 @@
         function shift_playlist() {
             plist_vl.shift()
             sd_wrapper.children[0].remove()
-            sd_wrapper.children[0].children[0].preload = "auto"
         }
-
 
         function ctrPlay(list,path) {
             if(list == currentPlay && !currentAudio.paused) return
@@ -335,7 +335,7 @@
                             curr_icon.pause()
                             console.log("icon done")
                             resolved()
-                        }, 200);
+                        }, 200)
                     }
                     else {
                         curr_icon.src = `${path}/${icon}`
@@ -346,7 +346,7 @@
                             curr_icon.pause()
                             console.log("icon done")
                             resolved()
-                        }, 200);
+                        }, 200)
                     }
                 } catch (err){
                     curr_icon.src = `${path}/${icon}`
@@ -432,6 +432,76 @@
         "PSYQUI - Education.mp4",
         "Zekk - City Lights (Remix).mp4",
         ],"Nyahn")
+
+        ctr_template_scroller([
+            "Bad Computer & Danyka Nadeau - Chasing Clouds.mp3",
+"Day by Day (feat Nicole Curry).mp3",
+"DoctorNoSense - Safe to Say (Official Audio).mp3",
+"kamome sano - Lovesick (feat ぷにぷに電機).mp3",
+"Just Love (feat PSYQUI).mp3",
+"Kunaru & Kachi - One More Time.mp3",
+"lapix - Day by day (PSYQUI Remix).mp3",
+"masara - Tegami (ft yutsuki & Hikaru Station).mp3",
+"Moe Shop - Baby Pink (w YUCe).mp3",
+"Kunaru & MG5902 - Hurt You.mp3",
+"Moe Shop - Notice (w TORIENA).mp3",
+"Nightcore - No Friends (Lyrics).mp3",
+"Moonstar88 - Migraine (Japanese Version)  kena & miyuki.mp3",
+"PSYQUI & Houou Karin - Shining Lights  Speed Garage.mp3",
+"PSYQUI - Are You Kidding Me (ft Mami)  Jpop Future Core.mp3",
+"PSYQUI - Beautiful Future.mp3",
+"f(x) - 4 Walls (Zekk Remix).mp3",
+"PSYQUI - Chatroom  Jpop Future Core.mp3",
+"PSYQUI - DJ Noriken - スターゲイザ (ft YUCe)  PSYQUI Remix.mp3",
+"PSYQUI - Dont You Want Me (ft SUCH)  Jpop Kawaii Future Bass 2019.mp3",
+"Psyqui - Deep Blue.mp3",
+"PSYQUI - dont you want me ft Such.mp3",
+"PSYQUI - Dont You Want Me (ft Such) Mameyudoufu Remix.mp3",
+"PSYQUI - Education.mp3",
+"PSYQUI - Education  Future Core 2019.mp3",
+"PSYQUI - Fly to the moon feat 中村さんそ.mp3",
+"PSYQUI - Fly To The Moon (ft 中村さんそ)  J-Pop.mp3",
+"PSYQUI - Hype (Lapix Remix) ft Such  Jcore Future Core.mp3",
+"PSYQUI - Funk Assembly  J-Pop Funk.mp3",
+"PSYQUI - Hype (ft SUCH)  Jpop Kawaii J-Core 2019.mp3",
+"PSYQUI - Multitalents.mp3",
+"PSYQUI - Love & Roll (ft SUCH) Speed Garage J-Pop.mp3",
+"PSYQUI - Mend Your Ways (ft SUCH)  Jpop Kawaii Future Bass 2019.mp3",
+"PSYQUI - OYASUMI in my dream.mp3",
+"PSYQUI - Rainbow Dream (ft Mo∀)  Future Core.mp3",
+"PSYQUI - No One  Future Core.mp3",
+"PSYQUI - Raise Your Hands (ft Such)  Future Core.mp3",
+"PSYQUI - Secret Dance Hall (ft SUCH)  J-pop 2019.mp3",
+"PSYQUI - Vital Error (Beat)  Future Core 2019.mp3",
+"PSYQUI - Hype feat Such.mp3",
+"PSYQUI - Your Voice So Zekk Full Spec Remix (Ft SUCH)  Future Core.mp3",
+"PSYQUI - ヒカリの方へ (ft SUCH)  Jpop.mp3",
+"PSYQUI - Your voice So M-Project Remix.mp3",
+"PSYQUI - センチメンタルハートボーイ(ft Such)  J-Pop Future Core.mp3",
+"PSYQUI - ヒカリの方へ - Lapix & Cranky Remix (ft Such)  Future Core.mp3",
+"PSYQUI - ヒステリックナイトガール (Awakening) (ft SUCH)  J-Pop.mp3",
+"PSYQUI - ヒステリックナイトガール feat Such (android52 Edit).mp3",
+"PSYQUI - 就寝御礼 (Singed By Psyqui Himself!)  J-Pop.mp3",
+"PSYQUI - ヒカリの方へ (ft SUCH) Awakening  Future Core.mp3",
+"PSYQUI feat Marpril - Girly Cupid.mp3",
+"Purukichi - LOOP  (Feat セトナツメ).mp3",
+"Purukichi - Mysterious feat pinana.mp3",
+"SPY x FAMILY - Ending Full「Comedy」by Gen Hoshino (LyricsSubtitles).mp3",
+"Signal (feat Such).mp3",
+"PSYQUI - Up n Up  Future Core.mp3",
+"Still in my heart (feat ぷにぷに電機).mp3",
+"Your voice so (feat such).mp3",
+"tekalu - Letter feat おんた.mp3",
+"YUCe x Snails House - Cosmic Air Ride.mp3",
+"Zakku x Nakanojojo - Matcha Love (feat アリガトユイナ).mp3",
+"[Blue Archive] Theme 109.mp3",
+"Zekk - City Lights (Remix).mp3",
+"「Footwork」[PSYQUI] Too Late.mp3",
+"「Future Core」[PSYQUI feat Such] ヒカリの方へ (Awakening).mp3",
+"高尾大毅 - Dont be afraid.mp3",
+"べに／可不.mp3",
+"PSYQUI ft Such - LOVE&ROLL.mp3"
+            ],"https://rcph-smz.github.io/smz/mp3","Main_Page")
         
 //         ctr_template_scroller([
 // "1_Hour_Most_Popular_Songs_by_PSYQUI_(NON-STOP_Collection_Vol._1_+_BONUS_TRACK)(720p).mp4",
