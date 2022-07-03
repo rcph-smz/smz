61918dbb1880b366b8630159cc7d6aa04644c3d7
rcph <rcph110110@gmail.com>
2022-07-03 16:13:43 +0800

    future_cake

--- module/node_js/list_managers/image_lister/app.js
+++ module/node_js/list_managers/image_lister/app.js
@@ -3,7 +3,7 @@ const fs = require('fs')
 const require_ext = ['png','jpeg','jpg']
 let appendTo = `image_list.txt`
 
-function inspect(path,nested_path) {
+function array_format(path,nested_path) {
 
     if(nested_path){
         path = `${path}/${nested_path}`
@@ -11,7 +11,7 @@ function inspect(path,nested_path) {
     fs.readdir(path,(serr,files) => {
         for(file of files){
             if(fs.statSync(`${path}/${file}`).isDirectory()) {
-                inspect(path,file)
+                array_format(path,file)
             }
             else if(fs.statSync(`${path}/${file}`).isFile()) {
                 for(ext of require_ext){
@@ -21,10 +21,10 @@ function inspect(path,nested_path) {
                     if(ext == file_array[file_array.length - 1]){
                         let appendText
                         if(path.includes("../")) {
-                            appendText = `"${path.slice(3,path.length - 1)}/${file.trim()}", \n`
+                            appendText = `"${path.slice(3,path.length)}/${file.trim()}", \n`
                         }
                         else if(path.includes("./")) {
-                            appendText = `"${path.slice(2,path.length - 1)}/${file.trim()}", \n`
+                            appendText = `"${path.slice(2,path.length)}/${file.trim()}", \n`
                         }
                         else {
                             appendText = `"${path}/${file.trim()}", \n`
@@ -41,7 +41,46 @@ function inspect(path,nested_path) {
         }
     })
 }
-inspect(".")
+
+function plain_format(path,nested_path) {
+
+    if(nested_path){
+        path = `${path}/${nested_path}`
+    }
+    fs.readdir(path,(serr,files) => {
+        for(file of files){
+            if(fs.statSync(`${path}/${file}`).isDirectory()) {
+                plain_format(path,file)
+            }
+            else if(fs.statSync(`${path}/${file}`).isFile()) {
+                for(ext of require_ext){
+
+                    file_array = file.split(".")
+
+                    if(ext == file_array[file_array.length - 1]){
+                        let appendText
+                        if(path.includes("../")) {
+                            appendText = `${path.slice(3,path.length)}/${file.trim()} \n`
+                        }
+                        else if(path.includes("./")) {
+                            appendText = `${path.slice(2,path.length)}/${file.trim()} \n`
+                        }
+                        else {
+                            appendText = `${path}/${file.trim()} \n`
+                        }
+                        
+
+                        fs.appendFile(appendTo,appendText,(err) => {
+
+                        })
+                    }
+                }
+                
+            }
+        }
+    })
+}
+plain_format(".")
 
 
 
