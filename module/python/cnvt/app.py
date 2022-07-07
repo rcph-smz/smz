39fd44a957eb1d7b012dd4ef66a11e251b441d8a
rcph <rcph110110@gmail.com>
2022-07-08 07:50:31 +0800

    done

--- module/python/cnvt/app.py
+++ module/python/cnvt/app.py
@@ -2,14 +2,19 @@ from moviepy.editor import *
 import os
 
 
-print("<------------- folderName <mp4 convert_to mp3> ------------->")
-folderName = str(input())
+print("<------------- folderName <mp4 convert_to mp3/find folder> ------------->")
+folderName = str(input()).strip()
+print("<------------- folderName <export/where to save> ------------->")
+pathName = str(input()).strip()
 
-for i in os.listdir(folderName): 
-    path = "MaidCat/"
-    videoclip = VideoFileClip(folderName + '/' + i)
-    audiofile = videoclip.audio.write_audiofile(i[:-4]+".mp3")
+for i in os.listdir(folderName):
+    path = f'{pathName}'
+    videoclip = VideoFileClip(f'{folderName}/{i}')
     if not os.path.exists(path):
         os.mkdir(path)
-    os.rename(i[:-4]+".mp3",path+i[:-4]+".mp3")
 
+    if os.path.isfile(f'{path}/{i[:-4]}.mp3'):
+        print(f'file exists at : {path}/{i[:-4]}.mp3')
+    else:
+        audiofile = videoclip.audio.write_audiofile(f'{i[:-4]}.mp3')
+        os.rename(f'{i[:-4]}.mp3',f'{path}/{i[:-4]}.mp3')
