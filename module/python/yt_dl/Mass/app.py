714d2b5fceb2ae9a70f97a2b2d8612cea90272c8
rcph <rcph110110@gmail.com>
2022-07-08 08:01:45 +0800

    done

--- module/python/yt_dl/Mass/app.py
+++ module/python/yt_dl/Mass/app.py
@@ -1,10 +1,12 @@
 from pytube import YouTube
 
 print("<------------- textfile name <youtubelinks> ------------->")
-file = str(input())
+file = str(input()).strip()
+print("<------------- where to save/folderName ------------->")
+path = str(input()).strip()
 with open(file,'r') as f:
     for i in f.readlines():
         yt = YouTube(i)
         print(yt.title)
         yt.thumbnail_url
-        yt.streams.get_highest_resolution().download("./MaidCat")
+        yt.streams.get_highest_resolution().download(f'./{path}')
--- module/python/yt_dl/Single/app.py
+++ module/python/yt_dl/Single/app.py
@@ -1,8 +1,10 @@
 from pytube import YouTube
 
 print("<------------- video url ------------->")
-url = input()
+url = str(input()).strip()
+print("<------------- where to save/folderName ------------->")
+path = str(input()).strip()
 yt = YouTube(url)
 print(yt.title)
 yt.thumbnail_url 
-yt.streams.get_highest_resolution().download("./MaidCat")
+yt.streams.get_highest_resolution().download(f'./{path}')
